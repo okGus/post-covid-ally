a590f0907c796d4c653bbff9591dd1998025911b
Gustavo <rosalesagustavo@gmail.com>
2021-08-23 21:26:43 -0400

    chat stuff

--- post-covid-ally/src/components/Chat.js
+++ post-covid-ally/src/components/Chat.js
@@ -1,11 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase'
 import SignOut from './SignOut'
 
 function Chat() {
+    const [messages, setMessages] = useState([])
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
     return (
         <div>
-            <SignOut></SignOut>
-            
+            <SignOut />
+            {messages.map(({id, text, photoURL}) => (
+                <div key={id}>
+                    <img src={photoURL} alt=""/>
+                    <p>{text}</p>
+                </div>
+            ))}
         </div>
     )
 }
