d5b8211f4e935ba2f12b98b330992e61bb2ebf0f
Gustavo <rosalesagustavo@gmail.com>
2021-08-25 16:51:19 -0400

    chatbot

--- post-covid-ally/src/App.js
+++ post-covid-ally/src/App.js
@@ -1,20 +1,11 @@
+import React from 'react';
 import './App.css';
-import Chat from './components/Chat';
 import Signin from './components/SignIn';
 import {auth} from './firebase.js'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import Navigation from './components/Navigation';
 import Account from './components/Account'
-///
-import React from 'react';
-import Chatbot from 'react-chatbot-kit'
-import './App.css';
-
-import ActionProvider from './ActionProvider';
-import MessageParser from './MessageParser';
-import config from './config';
-///
-
+import ChatBot from './components/ChatBot'
 
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 function App() {
@@ -30,14 +21,9 @@ function App() {
             {user ? <Account /> : <Redirect to="/signin"/>}
           </Route>
           <Route exact path="/">
-              {user ? <Chat /> : <Redirect to="/signin"/>}
+              {user ? <ChatBot /> : <Redirect to="/signin"/>}
           </Route>
         </Switch>
-      </div>
-      <div className="App">
-        <header className="App-header">
-          <Chatbot config={config} actionProvider={ActionProvider} 	    messageParser={MessageParser} />
-        </header>
       </div>
     </Router>
     
