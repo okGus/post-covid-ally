f7e9ff0015feb9c42dfcd336164d638ce95ec40e
hassanamjad1 <haxxter1977@gmail.com>
2021-08-25 14:51:04 -0400

    configuring react-chatbot-kit

--- post-covid-ally/src/App.js
+++ post-covid-ally/src/App.js
@@ -5,6 +5,17 @@ import {auth} from './firebase.js'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import Navigation from './components/Navigation';
 import Account from './components/Account'
+///
+import React from 'react';
+import Chatbot from 'react-chatbot-kit'
+import './App.css';
+
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './config';
+///
+
+
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 function App() {
   const [user] = useAuthState(auth)
@@ -22,6 +33,11 @@ function App() {
               {user ? <Chat /> : <Redirect to="/signin"/>}
           </Route>
         </Switch>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <Chatbot config={config} actionProvider={ActionProvider} 	    messageParser={MessageParser} />
+        </header>
       </div>
     </Router>
     
